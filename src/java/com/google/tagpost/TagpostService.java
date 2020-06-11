7a6e23b2e79f6413d2e8932a4fab1480661009a5
Jiayi Bao <jybao@google.com>
2020-06-11 22:41:26 +0000

    change rpc naming

--- src/java/com/google/tagpost/TagpostService.java
+++ src/java/com/google/tagpost/TagpostService.java
@@ -1,23 +1,21 @@
 package com.google.tagpost;
 
+import com.google.tagpost.FetchMessageRequest;
+import com.google.tagpost.FetchMessageResponse;
 import com.google.tagpost.TagpostServiceGrpc;
-import com.google.tagpost.SimpleReplyRequest;
-import com.google.tagpost.SimpleReplyResponse;
 import io.grpc.stub.StreamObserver;
-
-import java.io.IOException;
 
 public final class TagpostService extends TagpostServiceGrpc.TagpostServiceImplBase {
 
     @Override
-    public void simpleReply(SimpleReplyRequest req, StreamObserver<SimpleReplyResponse> responseObserver) {
-        SimpleReplyResponse response = determineResponse(req);
+    public void fetchMessage(FetchMessageRequest req, StreamObserver<FetchMessageResponse> responseObserver) {
+        FetchMessageResponse response = determineResponse(req);
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 
-    private static SimpleReplyResponse determineResponse(SimpleReplyRequest req) {
-        SimpleReplyResponse response = SimpleReplyResponse.newBuilder().setMessage("Hello " + req.getRequestId()).build();
+    private static FetchMessageResponse determineResponse(FetchMessageRequest req) {
+        FetchMessageResponse response = FetchMessageResponse.newBuilder().setMessage("Request received.").build();
         return response;
     }
 }
