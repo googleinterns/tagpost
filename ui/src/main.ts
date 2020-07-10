5d2902ebe724bb0adbf9290cffb785f4e9976602
Zehua Xu <xuzehua@google.com>
2020-07-10 16:59:37 -0700

    Add proto + gRPC to UI.

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { Thread } from "compiled_proto/src/proto/tagpost_pb";
+
+// Only for demo purpose. Can be removed after we have real protobuf use cases.
+const thread = new Thread();
+thread.setThreadId("12345");
+const serialized = thread.serializeBinary();
+console.log(serialized);
 
 if (environment.production) {
   enableProdMode();
