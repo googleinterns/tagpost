110a372f4ade28a840ddf2cc73e74a2640538728
Jiayi Bao <jybao@google.com>
2020-07-20 20:56:16 +0000

    update parent component to support modal

--- ui/src/app/data.service.ts
+++ ui/src/app/data.service.ts
@@ -1,15 +1,26 @@
 import {Injectable} from '@angular/core';
-import {environment} from '../environments/environment';
 
-import {TagpostServiceClient} from 'compiled_proto/src/proto/Tagpost_rpcServiceClientPb';
-import {FetchThreadsByTagRequest, FetchThreadsByTagResponse} from 'compiled_proto/src/proto/tagpost_rpc_pb';
 import {Observable, Subject} from 'rxjs';
 
+import {TagpostServiceClient} from 'compiled_proto/src/proto/Tagpost_rpcServiceClientPb';
+import {
+  AddThreadWithTagRequest,
+  AddThreadWithTagResponse,
+  FetchThreadsByTagRequest,
+  FetchThreadsByTagResponse
+} from 'compiled_proto/src/proto/tagpost_rpc_pb';
+import {Tag, Thread} from '../compiled_proto/src/proto/tagpost_pb';
+import {environment} from '../environments/environment';
+
+/**
+ * A data service that communicate with backend services.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   private client: TagpostServiceClient;
+
   private threadListSource = new Subject<any>();
   public readonly threadList: Observable<any> = this.threadListSource.asObservable();
 
@@ -17,6 +28,10 @@ export class DataService {
     this.client = new TagpostServiceClient(environment.apiProxy);
   }
 
+  /**
+   * Fetch a list of thread with given tag name.
+   * If success, Multicast the newly fetched thread list to all thread list observers
+   */
   fetchThreads(tag: string): void {
     const req = new FetchThreadsByTagRequest();
     req.setTag(tag);
@@ -28,5 +43,31 @@ export class DataService {
         this.threadListSource.next(response.toObject().threadsList);
       }
     });
+  }
+
+  /**
+   * Add a new thread with given tag name.
+   */
+  addThread(tag: string): Promise<Thread> {
+    return new Promise<any>(((resolve, reject) => {
+      const primaryTag = new Tag();
+      primaryTag.setTagName(tag);
+
+      const newThread = new Thread();
+      newThread.setPrimaryTag(primaryTag);
+
+      const req = new AddThreadWithTagRequest();
+      req.setThread(newThread);
+
+      this.client.addThreadWithTag(req, {}, (err, response: AddThreadWithTagResponse) => {
+        if (err) {
+          console.error(err.code, err.message);
+          reject(err);
+        }
+        if (response) {
+          resolve(response.toObject().thread);
+        }
+      });
+    }));
   }
 }
