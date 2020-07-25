5d8e38d06d88dfc7e0b46071735aaa2a4804d2ab
Jiayi Bao <jybao@google.com>
2020-07-25 01:21:28 +0000

    create thread-detail component to show comments and tagStat

---
+++ ui/src/app/panel/thread-detail/thread-detail.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-thread-detail',
+  templateUrl: './thread-detail.component.html',
+  styleUrls: ['./thread-detail.component.sass']
+})
+export class ThreadDetailComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
