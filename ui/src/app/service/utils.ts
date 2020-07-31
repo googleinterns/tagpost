1768aec994f51bce4ff0c293d521cde62a5feb5d
Jiayi Bao <jybao@google.com>
2020-07-31 01:24:35 +0000

    use locales to format Date

--- ui/src/app/service/utils.ts
+++ ui/src/app/service/utils.ts
@@ -7,6 +7,5 @@
  */
 export function convertTimestamp(timestamp: any): string {
   const date = new Date(timestamp.getSeconds() * 1000 + timestamp.getNanos() / 1000);
-  const month = date.toLocaleString('default', {month: 'long'});
-  return month + ' ' + date.getUTCDate() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes();
+  return date.toLocaleString('en-US', { timeZone: 'UTC' });
 }
