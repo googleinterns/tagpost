e2941b2df2e7efedbfdc80d7aa9a83d8ae91a118
Jiayi Bao <jybao@google.com>
2020-07-31 00:41:13 +0000

    refactor utils

--- ui/src/app/service/utils.ts
+++ ui/src/app/service/utils.ts
@@ -1,22 +1,12 @@
-import {Injectable} from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
 /**
  * Common methods to be used cross all components
  */
-export class UtilsService {
 
-  constructor() {
-  }
-
-  /**
-   * Convert proto timestamp to readable string
-   */
-  convertTimestamp(timestamp: any): string {
-    const date = new Date(timestamp.getSeconds() * 1000 + timestamp.getNanos() / 1000);
-    const month = date.toLocaleString('default', {month: 'long'});
-    return month + ' ' + date.getUTCDate() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes();
-  }
+/**
+ * Convert proto timestamp to readable string
+ */
+export function convertTimestamp(timestamp: any): string {
+  const date = new Date(timestamp.getSeconds() * 1000 + timestamp.getNanos() / 1000);
+  const month = date.toLocaleString('default', {month: 'long'});
+  return month + ' ' + date.getUTCDate() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes();
 }
