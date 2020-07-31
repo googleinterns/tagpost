fa44bac869c3395b8c2557a7ca968afa9c2c519c
Jiayi Bao <jybao@google.com>
2020-07-31 17:09:40 +0000

    modify observable's subscription to threadListSource

--- ui/src/app/panel/thread-list/thread-list.component.ts
+++ ui/src/app/panel/thread-list/thread-list.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
 
 import {Observable} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
 
 import {DataService} from 'app/service/data.service';
 import {convertTimestamp} from 'app/service/utils';
@@ -13,18 +11,14 @@ import {Thread} from 'compiled_proto/src/proto/tagpost_pb';
   styleUrls: ['./thread-list.component.sass']
 })
 export class ThreadListComponent implements OnInit {
+  // An observable stream that listen to values multicasted from threadList Subject
   threadList$: Observable<Array<Thread>>;
   convertTimestamp = convertTimestamp;
 
-  constructor(private dataService: DataService,
-              private route: ActivatedRoute) {
+  constructor(private dataService: DataService) {
   }
 
   ngOnInit(): void {
-    this.threadList$ = this.route.paramMap.pipe(
-      switchMap(params => {
-        return this.dataService.threadList;
-      })
-    );
+    this.threadList$ = this.dataService.threadList;
   }
 }
