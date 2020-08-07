488a34845262261539ec8e258df13fbc805f49cf
Jiayi Bao <jybao@google.com>
2020-08-07 00:35:32 +0000

    retrieve primaryTag from Thread Table when adding new comment

--- src/java/com/google/tagpost/spanner/SpannerService.java
+++ src/java/com/google/tagpost/spanner/SpannerService.java
@@ -99,6 +99,18 @@ public class SpannerService implements DataService {
     String commentId = UUID.randomUUID().toString();
     Timestamp timestamp = Timestamp.now();
 
+    // Retrieve primaryTagName from Thread table
+    String primaryTagName = "";
+    String SQLStatement = "SELECT PrimaryTag FROM Thread WHERE ThreadID = @threadId";
+    Statement statement =
+        Statement.newBuilder(SQLStatement).bind("threadId").to(comment.getThreadId()).build();
+
+    try (ResultSet resultSet = dbClient.singleUse().executeQuery(statement)) {
+      while (resultSet.next()) {
+        primaryTagName = resultSet.getString("PrimaryTag");
+      }
+    }
+
     Mutation mutation =
         Mutation.newInsertBuilder("Comment")
             .set("CommentID")
@@ -112,7 +124,7 @@ public class SpannerService implements DataService {
             .set("ThreadID")
             .to(comment.getThreadId())
             .set("PrimaryTag")
-            .to(comment.getPrimaryTag().getTagName())
+            .to(primaryTagName)
             .set("ExtraTags")
             .toStringArray(
                 comment.getExtraTagsList().stream()
