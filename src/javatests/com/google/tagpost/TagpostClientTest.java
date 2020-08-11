34d6362828d7db775cd14254435744b177a976f7
Jiayi Bao <jybao@google.com>
2020-08-11 05:18:47 +0000

    update client test

--- src/javatests/com/google/tagpost/TagpostClientTest.java
+++ src/javatests/com/google/tagpost/TagpostClientTest.java
@@ -5,7 +5,6 @@ import static org.mockito.AdditionalAnswers.delegatesTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import io.grpc.ManagedChannel;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.stub.StreamObserver;
@@ -18,24 +17,59 @@ import org.junit.runners.JUnit4;
 import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 
-
 /** Unit tests for {@link TagpostClient} */
 @RunWith(JUnit4.class)
 public class TagpostClientTest {
-  /** Automatic graceful shutdown registered servers and channels at the end of test. */
+
+  static final String TAG = "tag";
+  static final String THREAD_ID = "000";
+
+  // Automatic graceful shutdown registered servers and channels at the end of test.
   @Rule public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
-  // Implement a fake service
+
+  // Implement a server with fake services
   private final TagpostServiceGrpc.TagpostServiceImplBase serviceImpl =
       mock(
           TagpostServiceGrpc.TagpostServiceImplBase.class,
           delegatesTo(
               new TagpostServiceGrpc.TagpostServiceImplBase() {
                 @Override
-                public void fetchMessage(
-                    FetchMessageRequest request,
-                    StreamObserver<FetchMessageResponse> respObserver) {
-                  respObserver.onNext(FetchMessageResponse.getDefaultInstance());
+                public void fetchThreadsByTag(
+                    FetchThreadsByTagRequest request,
+                    StreamObserver<FetchThreadsByTagResponse> respObserver) {
+                  respObserver.onNext(FetchThreadsByTagResponse.getDefaultInstance());
                   respObserver.onCompleted();
+                }
+
+                @Override
+                public void addThreadWithTag(
+                    AddThreadWithTagRequest req,
+                    StreamObserver<AddThreadWithTagResponse> responseObserver) {
+                  responseObserver.onNext(AddThreadWithTagResponse.getDefaultInstance());
+                  responseObserver.onCompleted();
+                }
+
+                @Override
+                public void fetchCommentsUnderThread(
+                    FetchCommentsUnderThreadRequest req,
+                    StreamObserver<FetchCommentsUnderThreadResponse> responseObserver) {
+                  responseObserver.onNext(FetchCommentsUnderThreadResponse.getDefaultInstance());
+                  responseObserver.onCompleted();
+                }
+
+                @Override
+                public void addCommentUnderThread(
+                    AddCommentUnderThreadRequest req,
+                    StreamObserver<AddCommentUnderThreadResponse> responseObserver) {
+                  responseObserver.onNext(AddCommentUnderThreadResponse.getDefaultInstance());
+                  responseObserver.onCompleted();
+                }
+
+                @Override
+                public void getTagStats(
+                    GetTagStatsRequest req, StreamObserver<GetTagStatsResponse> responseObserver) {
+                  responseObserver.onNext(GetTagStatsResponse.getDefaultInstance());
+                  responseObserver.onCompleted();
                 }
               }));
 
@@ -54,26 +88,82 @@ public class TagpostClientTest {
             .build()
             .start());
 
-    // Create a client channel and register for automatic graceful shutdown.
-    ManagedChannel channel =
-        grpcCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build());
-
-    // Create a HelloWorldClient using the in-process channel;
-    client = new TagpostClient(channel);
+    // Create a client using the in-process channel;
+    client =
+        new TagpostClient(
+            grpcCleanup.register(
+                InProcessChannelBuilder.forName(serverName).directExecutor().build()));
   }
 
-  /** Call from the client against the fake server */
   @Test
-  public void communicate_deliversMessageToServer() {
-    ArgumentCaptor<FetchMessageRequest> requestCaptor =
-        ArgumentCaptor.forClass(FetchMessageRequest.class);
+  public void requestFetchThreads_success() {
 
-    String request_id = "111";
-    client.communicate(request_id);
+    // Call from the client against the fake server
+    ArgumentCaptor<FetchThreadsByTagRequest> requestCaptor =
+        ArgumentCaptor.forClass(FetchThreadsByTagRequest.class);
+    client.requestFetchThreads(TAG);
+
+    // Verify behaviors or state changes from the server side.
+    verify(serviceImpl)
+        .fetchThreadsByTag(
+            requestCaptor.capture(),
+            ArgumentMatchers.<StreamObserver<FetchThreadsByTagResponse>>any());
+    assertEquals(TAG, requestCaptor.getValue().getTag());
+  }
+
+  @Test
+  public void requestAddNewThread_success() {
+
+    ArgumentCaptor<AddThreadWithTagRequest> requestCaptor =
+        ArgumentCaptor.forClass(AddThreadWithTagRequest.class);
+    client.requestAddNewThread(THREAD_ID);
 
     verify(serviceImpl)
-        .fetchMessage(
-            requestCaptor.capture(), ArgumentMatchers.<StreamObserver<FetchMessageResponse>>any());
-    assertEquals(request_id, requestCaptor.getValue().getRequestId());
+        .addThreadWithTag(
+            requestCaptor.capture(),
+            ArgumentMatchers.<StreamObserver<AddThreadWithTagResponse>>any());
+    assertEquals(THREAD_ID, requestCaptor.getValue().getThread().getPrimaryTag().getTagName());
   }
+
+  @Test
+  public void requestFetchComments_success() {
+
+    ArgumentCaptor<FetchCommentsUnderThreadRequest> requestCaptor =
+        ArgumentCaptor.forClass(FetchCommentsUnderThreadRequest.class);
+    client.requestFetchComments(THREAD_ID);
+
+    verify(serviceImpl)
+        .fetchCommentsUnderThread(
+            requestCaptor.capture(),
+            ArgumentMatchers.<StreamObserver<FetchCommentsUnderThreadResponse>>any());
+    assertEquals(THREAD_ID, requestCaptor.getValue().getThreadId());
+  }
+
+  @Test
+  public void requestAddNewComment_success() {
+
+    ArgumentCaptor<AddCommentUnderThreadRequest> requestCaptor =
+        ArgumentCaptor.forClass(AddCommentUnderThreadRequest.class);
+    client.requestAddNewComment(THREAD_ID);
+
+    verify(serviceImpl)
+        .addCommentUnderThread(
+            requestCaptor.capture(),
+            ArgumentMatchers.<StreamObserver<AddCommentUnderThreadResponse>>any());
+    assertEquals(THREAD_ID, requestCaptor.getValue().getComment().getThreadId());
+  }
+
+    @Test
+    public void requestGetTagStats_success() {
+
+        ArgumentCaptor<GetTagStatsRequest> requestCaptor =
+                ArgumentCaptor.forClass(GetTagStatsRequest.class);
+        client.requestGetTagStats(TAG);
+
+        verify(serviceImpl)
+                .getTagStats(
+                        requestCaptor.capture(),
+                        ArgumentMatchers.<StreamObserver<GetTagStatsResponse>>any());
+        assertEquals(TAG, requestCaptor.getValue().getTag());
+    }
 }
